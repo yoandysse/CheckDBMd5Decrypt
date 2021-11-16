a7a7b53545b75534f6cd431bf4e7c745a1f7c5f3
yoandysse <yoandysse@gmail.com>
2021-11-16 18:06:23 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='CheckDBMd5Decrypt',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/yoandysse/CheckDBMd5Decrypt',
+    license='MIT License',
+    author='Yoandy Isse Oña',
+    author_email='yoandysse@gmail.com',
+    description='Este script permite buscar hashes en la web md5decrypt.net a través de su API.  '
+)
